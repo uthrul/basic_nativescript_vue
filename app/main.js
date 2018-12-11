65bf3b95b663b17ab1bedb18b6c95318e604eba5
saud maruli <uthrul@gmail.com>
2018-12-11 10:58:27 +0700

    Components Global

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 import Vue from 'nativescript-vue'
 
+import GroceryItem from './GroceryItem'
+
 import VueDevtools from 'nativescript-vue-devtools';
 Vue.use(VueDevtools);
 
@@ -8,32 +10,10 @@ new Vue({
     <Frame>
       <Page>
       <Stacklayout>
-        <TextField v-model="firstName"></TextField>
-        <TextField v-model="lastName"></TextField>
-        <TextField :text="getfullName()"></TextField>
+        <GroceryItem></GroceryItem>
       </Stacklayout>
       </Page>
     </Frame>
   `,
-  data:{
-    firstName:"",
-    lastName:""
-  },
-  methods: {
-    getfullName: function () {
-      return this.firstName + " " + this.lastName;
-    }
-  },
-  computed: {
-    fullName: {
-      get: function () {
-        return this.firstName + " " + this.lastName;
-      },
-      set: function (newValue) {
-        var names = newValue.split(' ');
-        this.firstName = names[0];
-        this.lastName = names[names.length - 1];
-      }
-    }
-  }
+  data: {}
 }).$start()
